a9a50c8e542776f30ed9ed2ad2b88f75688e1fae
Cliff Spital <cspital@uw.edu>
2019-05-01 08:26:52 -0700

    file magic to get apache working?

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from modules.app import app
+
---
+++ run.py
@@ -0,0 +1,19 @@
+''' index file for Flask API '''
+import os
+from modules.app import app
+from modules import logger
+
+application = app
+
+if __name__ == "__main__":
+    HOST = None
+    _containerd = os.environ.get('FLASK_RUNNING_IN_DOCKER')
+    if _containerd and _containerd == 'true':
+        HOST = '0.0.0.0'
+
+    PORT = os.environ.get('PORT')
+    if PORT:
+        PORT = int(PORT)
+    else:
+        PORT = 5001
+    app.run(host=HOST, port=PORT)
